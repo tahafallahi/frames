2ac7db33b17ec9dd8519177d7a86bda6731f3e7f
Taha Fallahi <tahafallahi.blob@gmail.com>
2026-08-17 17:47:34 +0330

    Add search logic to search bar

--- frontend/src/layouts/header/search-bar/search-bar.tsx
+++ frontend/src/layouts/header/search-bar/search-bar.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, type ChangeEvent, type Dispatch, type SetStateAction } from "react";
 
 import {
   Popover,
@@ -7,17 +7,44 @@ import {
   PopoverTitle,
 } from "@/components/ui/popover";
 import { Input } from "@/components/ui/input";
-import { mockSearchResult } from "@/testing/mocks/searchResults";
 import SlimCard from "@/components/slim-card/slim-card";
 import Profile from "@/components/profile-card/profile-card";
 import { MessageCircle, ThumbsUp } from "lucide-react";
+import { api } from "@/lib/api";
+import type { ApiSearchResponse } from "@/types/search";
+
+function getSearchResult(query: string, limit: number, setResult: Dispatch<SetStateAction<ApiSearchResponse>>) {
+  const result = api.get<ApiSearchResponse>(`/search?query=${query}&limit=${limit}`);
+  result
+    .then((response) => {setResult(response.data)})
+    .catch((error) => console.log(error));
+}
 
 export default function SearchBar() {
   const [query, setQuery] = useState("");
   const [open, setOpen] = useState(false);
+  const [response, setResult] = useState<ApiSearchResponse | null>();
   const inputRef = useRef(null);
+  const timeoutId = useRef<number>(null);
+  const limit = 3;
 
-  const result = mockSearchResult;
+  function handleInput(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.value) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+    setQuery(e.target.value);
+
+    if (timeoutId.current) clearTimeout(timeoutId.current);
+    timeoutId.current = setTimeout(
+      getSearchResult,
+      2000,
+      query,
+      limit,
+      setResult,
+    );
+  }
 
   return (
     <>
@@ -25,14 +52,7 @@ export default function SearchBar() {
         ref={inputRef}
         className="w-175 h-11 px-5 mx-10 border-primary rounded-full focus-visible:ring-ring"
         placeholder="Search"
-        onInput={(e) => {
-          if (e.currentTarget.value) {
-            setOpen(true);
-          } else {
-            setOpen(false);
-          }
-          setQuery(e.currentTarget.value);
-        }}
+        onChange={handleInput}
         onFocus={() => {
           if (query) setOpen(true);
         }}
@@ -59,45 +79,41 @@ export default function SearchBar() {
               <div className="flex-1">
                 <h3 className="text-base text-foreground">Movies & TV Shows</h3>
                 <div className="grid grid-cols-3 gap-2 py-3">
-                  {result.shows.map((s, i) => (
+                  {response?.movies.map((s, i) => (
                     <div key={i} className="bg-background">
-                      <img src={s.img} alt={`Poster for the Show ${s.name}`} />
-                      <p className="text-base text-center py-1">{s.name}</p>
+                      <img src={s.poster_path} alt={`Poster for the Show ${s.title}`} />
+                      <p className="text-base text-center py-1">{s.title}</p>
                     </div>
                   ))}
                 </div>
               </div>
               <div className="w-60 flex flex-col gap-3">
                 <h3 className="text-base text-foreground">Users</h3>
-                <div className="flex flex-col gap-2">
-                  {result.users.map((u, i) => (
+                {/* <div className="flex flex-col gap-2">
+                  {response?.users.map((u, i) => (
                     <SlimCard key={i}>
                       <Profile user={u} variant={"compact"} />
                     </SlimCard>
                   ))}
-                </div>
+                </div> */}
               </div>
             </div>
             <hr className="my-3 border-border" />
             <div className="w-full flex flex-col gap-3">
               <h3 className="text-base text-foreground">Posts</h3>
               <div className="flex flex-col gap-2">
-                {result.posts.map((p, i) => (
+                {response?.posts.map((p, i) => (
                   <SlimCard key={i} className="flex">
                     <div className="flex-1">
                       <p className="text-foreground text-xl ">{p.title}</p>
                       <p>
-                        {p.show.type}: {p.show.name}
+                        {p.showMediaType}: {p.showTitle}
                       </p>
                     </div>
                     <div className="w-20">
                       <p className="flex gap-3 items-center text-sm">
                         <ThumbsUp className="w-4" />
                         {p.likes < 1000 ? p.likes : p.likes / 1000 + "k"}
-                      </p>
-                      <p className="flex gap-3 items-center text-sm">
-                        <MessageCircle className="w-4" />
-                        {p.commentsCount}
                       </p>
                     </div>
                   </SlimCard>
---
+++ frontend/src/types/search.ts
@@ -0,0 +1,32 @@
+export interface ApiSearchResponse {
+  users: ApiSearchUser[];
+  posts: ApiSearchPost[];
+  movies: ApiSearchMovie[];
+  tvs: ApiSearchTvShow[];
+}
+
+export interface ApiSearchUser {
+  id: string;
+  username: string;
+}
+
+export interface ApiSearchPost {
+  id: string;
+  title: string;
+  showMediaType: "MOVIE" | "TV_SHOW";
+  showTitle: string;
+  likes: number; 
+}
+
+export interface ApiSearchMovie {
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
+
+export interface ApiSearchTvShow {
+  id: number;
+  poster_path: string;
+  release_date: string;
+}
