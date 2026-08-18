2d43be36a4097e70161548f1da756123f495019b
Taha Fallahi <tahafallahi.blob@gmail.com>
2026-08-18 18:40:18 +0330

    Reimplemnt search logic with react query

--- frontend/src/layouts/header/search-bar/search-bar.tsx
+++ frontend/src/layouts/header/search-bar/search-bar.tsx
@@ -1,10 +1,5 @@
-import {
-  useRef,
-  useState,
-  type ChangeEvent,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import {
   Popover,
@@ -19,45 +14,45 @@ import { ThumbsUp } from "lucide-react";
 import { api } from "@/lib/api";
 import type { ApiSearchResponse } from "@/types/search";
 
-function getSearchResult(
-  query: string,
-  limit: number,
-  setResult: Dispatch<SetStateAction<ApiSearchResponse>>,
-) {
+async function getSearchResult(query: string, limit: number) {
   console.log("fired");
-  const result = api.get<ApiSearchResponse>(`/search?query=${query}`);
-  result
-    .then((response) => {
-      setResult(response.data);
-    })
-    .catch((error) => console.log(error));
+  const result = await api.get<ApiSearchResponse>(`/search?query=${query}`);
+
+  return result.data;
 }
 
 export default function SearchBar() {
-  const [query, setQuery] = useState("");
+  const [input, setInput] = useState("");
   const [open, setOpen] = useState(false);
-  const [response, setResult] = useState<ApiSearchResponse | null>();
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const timeoutId = useRef<number>(null);
   const limit = 3;
 
-  function handleInput(e: ChangeEvent<HTMLInputElement>) {
+  console.log(input);
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["searchResult", input],
+    queryFn: () => getSearchResult(input, limit),
+    staleTime: 1000 * 60,
+  });
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.value) {
       setOpen(true);
     } else {
       setOpen(false);
     }
-    setQuery(e.target.value);
 
     if (timeoutId.current) clearTimeout(timeoutId.current);
-    timeoutId.current = setTimeout(
-      getSearchResult,
-      600,
-      query,
-      limit,
-      setResult,
-    );
+    timeoutId.current = setTimeout(() => setInput(e.target.value), 500);
   }
+
+  // complet search bar
+  // move debvaouce time to config.ts
+  // consider diffrent buggs and edge caes for search bar
+  // implement diffrent state, pending, error and done
+
+  // All that needs attentions has been give attention. The work is done.
 
   return (
     <>
@@ -67,9 +62,11 @@ export default function SearchBar() {
         placeholder="Search"
         onChange={handleInput}
         onFocus={() => {
-          if (query) setOpen(true);
+          if (input) setOpen(true);
         }}
-        onBlur={() => setOpen(false)}
+        onBlur={() => {
+          setOpen(false);
+        }}
       ></Input>
 
       <Popover open={open}>
@@ -80,10 +77,11 @@ export default function SearchBar() {
           initialFocus={false}
           finalFocus={false}
           anchor={inputRef}
+          onMouseDown={(e) => e.preventDefault()}
         >
           <PopoverHeader>
             <PopoverTitle className="text-sm text-muted-foreground">
-              Search results for: {query}
+              Search results for: {input}
             </PopoverTitle>
             <hr className="my-1 border-border" />
           </PopoverHeader>
@@ -91,73 +89,108 @@ export default function SearchBar() {
             <div className="w-full flex gap-5">
               <div className="flex-1">
                 <h3 className="text-base text-foreground">Movies & TV Shows</h3>
-                <div className="grid grid-cols-3 gap-2 py-3">
-                  {response?.movies.map((s, i) => (
-                    <div key={i} className="bg-background">
-                      <img
-                        src={
-                          s.poster_path
-                            ? "https://image.tmdb.org/t/p/w200/" + s.poster_path
-                            : import.meta.env.VITE_MOVIE_PLACEHOLDER
-                        }
-                        alt={s.title}
-                      />
-                      <p className="text-sm text-center py-4">{s.title}</p>
+                {isError && `Something went wrong: ${error}`}
+                {isLoading && "Loading..."}
+                {data ? (
+                  data.movies.length > 1 ? (
+                    <div className="grid grid-cols-3 gap-2 py-3">
+                      {data.movies.map((s, i) => (
+                        <div key={i} className="bg-background">
+                          <img
+                          className="w-50"
+                            src={
+                              s.poster_path
+                                ? "https://image.tmdb.org/t/p/w154/" +
+                                  s.poster_path
+                                : import.meta.env.VITE_MOVIE_PLACEHOLDER
+                            }
+                            alt={s.title}
+                          />
+                          <p className="text-sm text-center py-4">{s.title}</p>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  ) : (
+                    `There was no movies matching ${input}`
+                  )
+                ) : null}
               </div>
               <div className="flex-1">
                 <h3 className="text-base text-foreground">TV Shows</h3>
-                <div className="grid grid-cols-3 gap-2 py-3">
-                  {response?.tvs.map((s, i) => (
-                    <div key={i} className="bg-background">
-                      <img
-                        src={
-                          s.poster_path
-                            ? "https://image.tmdb.org/t/p/w200/" + s.poster_path
-                            : import.meta.env.VITE_TV_SHOW_PLACEHOLDER
-                        }
-                        alt={s.title}
-                      />
-                      <p className="text-sm text-center py-4">{s.title}</p>
+                {isError && `Something went wrong: ${error}`}
+                {isLoading && "Loading..."}
+                {data ? (
+                  data.tvs.length > 1 ? (
+                    <div className="grid grid-cols-3 gap-2 py-3">
+                      {data.tvs.map((s, i) => (
+                        <div key={i} className="bg-background">
+                          <img className="w-50"
+                            src={
+                              s.poster_path
+                                ? "https://image.tmdb.org/t/p/w154/" +
+                                  s.poster_path
+                                : import.meta.env.VITE_TV_SHOW_PLACEHOLDER
+                            }
+                            alt={s.title}
+                          />
+                          <p className="text-sm text-center py-4">{s.title}</p>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  ) : (
+                    `There are no tv shows for ${input}`
+                  )
+                ) : null}
               </div>
               <div className="w-60 flex flex-col gap-3">
                 <h3 className="text-base text-foreground">Users</h3>
-                <div className="flex flex-col gap-2">
-                  {response?.users.map((u, i) => (
-                    <SlimCard key={i}>
-                      <Profile user={u} variant={"compact"} />
-                    </SlimCard>
-                  ))}
-                </div>
+                {isError && `Something went wrong: ${error}`}
+                {isLoading && "Loading..."}
+                {data ? (
+                  data.users.length > 1 ? (
+                    <div className="flex flex-col gap-2">
+                      {data.users.map((u, i) => (
+                        <SlimCard key={i}>
+                          <Profile user={u} variant={"compact"} />
+                        </SlimCard>
+                      ))}
+                    </div>
+                  ) : (
+                    `There are no users for ${input}`
+                  )
+                ) : null}
               </div>
             </div>
             <hr className="my-3 border-border" />
             <div className="w-full flex flex-col gap-3">
               <h3 className="text-base text-foreground">Posts</h3>
-              <div className="flex flex-col gap-2">
-                {response?.posts.map((p, i) => (
-                  <SlimCard key={i} className="flex">
-                    <div className="flex-1">
-                      <p className="text-foreground text-xl ">{p.title}</p>
-                      <p>
-                        {p.showMediaType === "MOVIE" ? "Movie" : "TV Show"}:{" "}
-                        {p.showTitle}
-                      </p>
-                    </div>
-                    <div className="w-20">
-                      <p className="flex gap-3 items-center text-sm">
-                        <ThumbsUp className="w-4" />
-                        {p.likes < 1000 ? p.likes : p.likes / 1000 + "k"}
-                      </p>
-                    </div>
-                  </SlimCard>
-                ))}
-              </div>
+              {isError && `Something went wrong: ${error}`}
+              {isLoading && "Loading..."}
+              {data ? (
+                data.posts.length > 1 ? (
+                  <div className="flex flex-col gap-2">
+                    {data.posts.map((p, i) => (
+                      <SlimCard key={i} className="flex">
+                        <div className="flex-1">
+                          <p className="text-foreground text-xl ">{p.title}</p>
+                          <p>
+                            {p.showMediaType === "MOVIE" ? "Movie" : "TV Show"}:{" "}
+                            {p.showTitle}
+                          </p>
+                        </div>
+                        <div className="w-20">
+                          <p className="flex gap-3 items-center text-sm">
+                            <ThumbsUp className="w-4" />
+                            {p.likes < 1000 ? p.likes : p.likes / 1000 + "k"}
+                          </p>
+                        </div>
+                      </SlimCard>
+                    ))}
+                  </div>
+                ) : (
+                  `There are no posts for ${input}`
+                )
+              ) : null}
             </div>
           </div>
         </PopoverContent>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import Layout from "./pages/layout/layout";
@@ -18,10 +19,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 document.documentElement.classList.add("dark");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
