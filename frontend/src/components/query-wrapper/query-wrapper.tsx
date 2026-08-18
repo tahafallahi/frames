75f0c938a12b28cc83e038f0609ed8db3202623d
Taha Fallahi <tahafallahi.blob@gmail.com>
2026-08-18 19:26:05 +0330

    Add query wrapper

---
+++ frontend/src/components/query-wrapper/query-wrapper.tsx
@@ -0,0 +1,23 @@
+interface props {
+  queryData: {
+    isError: boolean;
+    isLoading: boolean;
+    data?: unknown[];
+    error?: Error | null;
+  };
+  emptyStateMessage?: string | null;
+  children: React.ReactNode;
+}
+
+export default function QueryWrapper({
+  queryData,
+  emptyStateMessage = null,
+  children,
+}: props) {
+  if (queryData.isError) return <p>{`Something went wrong: ${queryData.error}`}</p>;
+  if (queryData.isLoading) return <p>Loading...</p>;
+  if (queryData.data) {
+    if (queryData.data.length > 0) return children;
+    if (emptyStateMessage) return <p>{emptyStateMessage}</p>;
+  }
+}
