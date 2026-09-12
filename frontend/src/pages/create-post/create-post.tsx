09960b11835c12c9e216e1ae71dce0bb06a33098
Taha Fallahi <tahafallahi.blob@gmail.com>
2026-09-12 11:35:44 +0330

    Fix create post

--- frontend/src/pages/create-post/create-post.tsx
+++ frontend/src/pages/create-post/create-post.tsx
@@ -11,30 +11,17 @@ import {
 } from "@/types/show";
 import { useQuery } from "@tanstack/react-query";
 import { useRef, useState } from "react";
-import { useSearchParams } from "react-router";
 
 const DEBOUNCE_DELAY = 300;
 
 export default function CreatePost() {
   const timeoutId = useRef<number>(null);
-  const [searchParams] = useSearchParams();
 
-  const showId = Number(searchParams.get("showId"));
-  const mediaType = searchParams.get("mediaType") as "MOVIE" | "TV_SHOW";
+  const [show, setShow] = useState<ShowIdentifier | null>(null);
 
-  const [show, setShow] = useState<ShowIdentifier | null>(
-    showId && mediaType
-      ? {
-          tmdbId: showId,
-          mediaType: (mediaType === "MOVIE" ? MediaType.MOVIE : MediaType.TV_SHOW),
-        }
-      : null,
-  );
-
-  console.log(typeof show!.tmdbId, typeof show!.mediaType);
 
   const showQuery = useQuery({
-    queryKey: ["show", show!.tmdbId, show!.mediaType],
+    queryKey: ["show", show?.tmdbId, show?.mediaType],
     queryFn: async () =>
       (
         await api.get<Show>(
@@ -65,31 +52,29 @@ export default function CreatePost() {
         />
       </div>
       <div>
-        {showQuery.isEnabled && show && (
-          <QueryWrapper
-            query={showQuery}
-            isEmpty={!!(showQuery.data && !Object.keys(showQuery.data).length)}
-            loadingPlaceHolder={
-              <div>
-                <Skeleton className="h-7 mb-4" />
-                <Skeleton className="ml-4 h-100" />
-                <div className="ml-4 mt-4 flex flex-col gap-2">
-                  <Skeleton variant="line" />
-                  <Skeleton variant="line" />
-                  <Skeleton variant="line" />
-                  <Skeleton variant="line" />
-                  <Skeleton variant="line" />
-                  <Skeleton variant="line" className="w-50" />
-                  <Skeleton variant="line" className="w-50" />
-                </div>
+        {showQuery.isEnabled && 
+        <QueryWrapper
+        query={showQuery}
+        isEmpty={!!(showQuery.data && !Object.keys(showQuery.data).length)}
+        loadingPlaceHolder={
+            <div>
+              <Skeleton className="h-7 mb-4" />
+              <Skeleton className="ml-4 h-100" />
+              <div className="ml-4 mt-4 flex flex-col gap-2">
+                <Skeleton variant="line" />
+                <Skeleton variant="line" />
+                <Skeleton variant="line" />
+                <Skeleton variant="line" />
+                <Skeleton variant="line" />
+                <Skeleton variant="line" className="w-50" />
+                <Skeleton variant="line" className="w-50" />
               </div>
-            }
+            </div>
+          }
           >
-            {showQuery.data && (
-              <ShowCard show={showQuery.data} title overview />
-            )}
-          </QueryWrapper>
-        )}
+          {showQuery.data && <ShowCard show={showQuery.data} title overview />}
+        </QueryWrapper>
+        }
       </div>
     </>
   );
